d94e36f180c8a0267cb39a12f8d560ccf0a75752
tsunao8989 <sunao.tsuyuki@arc-nect.co.jp>
2020-12-22 23:34:35 +0900

    typo

---
+++ depth-first-search/main.py
@@ -0,0 +1,26 @@
+from sys import stdin
+
+n = int(input())
+a = list(map(int, input().split(',')))
+k = int(input())
+
+def dfs(i, sum):
+    if i == n:
+        return sum == k
+
+    if dfs(i + 1, sum):
+        return True
+
+    if dfs(i + 1, sum + a[i]):
+        return True
+    
+    return False
+
+def main():
+    if dfs(0, 0):
+        print("Yes")
+    else:
+        print("No")
+
+if __name__ == "__main__":
+    main()
