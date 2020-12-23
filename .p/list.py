2624bf26535bc54d368e7db7b1aba88d63b0d6d3
tsunao8989 <sunao.tsuyuki@arc-nect.co.jp>
2020-12-23 22:00:21 +0900

    練習コードの削除

--- .p/list.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def main():
-    data = range(1, 1000)
-    c = len([x for x in data if x % 3 == 0])
-    print(c)
-if __name__ == "__main__":
-    main()
--- .p/sort_char.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def main():
-    a = ['e', 'B', 'd', 'C', 'a']
-    print(sorted(a))
-    print(sorted(a, key=lambda x: x.upper()))
-
-if __name__ == "__main__":
-    main()
